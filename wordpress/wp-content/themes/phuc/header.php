37ca5a440d1661ea6497f0a9ddfe936ef2012c77
John Davidson <xuz99@live.co.uk>
2020-11-15 23:25:23 +0000

    Installed TailwindCSS

--- wordpress/wp-content/themes/phuc/header.php
+++ wordpress/wp-content/themes/phuc/header.php
@@ -2,11 +2,13 @@
 <html>
 <head>
 	<?php wp_head(); ?>  
-	<title>Phuc Theme</title>
+	<title >Phuc Theme</title>
 </head>
-<body>
+<body class="bg-green-100">
 
-	<h1><?php bloginfo('name'); ?></h1>
+	<h1 class="text-6xl font-sans"><?php bloginfo('name'); ?></h1>
+	<h1>Hello</h1>
+
 	<p><?php bloginfo('description'); ?></p>
 
 	
